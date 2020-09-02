345b21f16d6a12e57ca19b61a5d74ebdeefb2e47
YOYO <xyz@gmail.com>
2020-09-02 19:16:51 +0530

    make it better

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ if(process.env.NODE_ENV === 'production'){
    app.use(express.static('frontend/build')) 
    const path = require('path')
    app.get('*',(req,res) => {
-       res.sendFile(path.resolve(__dirname,'../frontend','build','index.html'))
+       res.sendFile(path.resolve(__dirname,'client','build','index.html'))
    })
 }
 
