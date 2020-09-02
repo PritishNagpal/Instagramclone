e97d07a767156ef686e6a5ab13f61a2cbbe3a15d
YOYO <xyz@gmail.com>
2020-09-02 19:18:39 +0530

    make it better

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(userRouter);
 const PORT = process.env.PORT || 5000;
 
 if(process.env.NODE_ENV === 'production'){
-   app.use(express.static('frontend/build')) 
+   app.use(express.static('client/build')) 
    const path = require('path')
    app.get('*',(req,res) => {
        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
